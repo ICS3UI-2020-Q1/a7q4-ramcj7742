243f5df193b788796891a02bf4b14a7a4db99a3b
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-15 02:45:12 +0000

    Finishd last digit problem

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * prints the last digit of users number
+ * @author Jaden Ramcharan
  */
 public class Main {
+  //gets last digit
+  public static int lastDigit(int userNum){
+    int last = userNum % 10;
+    //prints and returns last digit
+    System.out.println(last);
+    return last;
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //gets number from user
+    int userNum = input.nextInt();
+
+    //shows last digit
+    lastDigit(userNum);
+
     
   }
 }
